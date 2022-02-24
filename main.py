487ceccd719ca621d93fd95a04b206ace2a5d4e8
Will Rages <90067249+WillRages@users.noreply.github.com>
2022-02-24 00:25:18 +0000

    Used a POST instead of a GET, also updates the values

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request
 import requests
 from forms import hotelForm
 import json
-import sqlite3
-from sqlite3 import Error
+import sqlalchemy 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "csgod"
@@ -47,13 +46,37 @@ def HotelPage():
            f.write(responseForHotel.text) #make sure you write to the file
     return render_template("hotelform.html", form=form)
 
-@app.route("/hotelout")
+@app.route("/hotelout",methods=["GET","POST"])
 def HotelOutput():
-    global hotelJSONdata
-    data = json.loads(hotelJSONdata)
-    places = []
-    for location in data["suggestions"][1]["entities"]:
-        places.append(location["name"])
-    return render_template("hotelOutput.html", Hotels = str(places))
+
+    
+    print(request.values)
+
+    if request.values.get("getHotels") == "y":
+
+        location = request.values.get("getPlace")
+
+        urlforhotels = "https://hotels4.p.rapidapi.com/locations/v2/search"
+
+        querystringforhotels = {
+            "query": location,  # you change the place name to your liking which is nice
+            "locale": "en_US",
+            "currency": "USD",
+        }
+
+        headersforhotel = {
+            "x-rapidapi-host": "hotels4.p.rapidapi.com",
+            "x-rapidapi-key": "4b44744771mshe890455adb53acdp173a42jsned37231cf971",
+        }
+
+        responseForHotel = requests.get(
+            urlforhotels, headers=headersforhotel, params=querystringforhotels
+        )
+        
+        hotelJSONdata = responseForHotel.json()  # Do stuff with this data
+
+        #with open("hotelData.json", "w+") as f:
+        #   f.write(responseForHotel.text) #make sure you write to the file
+    return render_template("hotelOutput.html", Hotels = str(hotelJSONdata))
 
 app.run(host="0.0.0.0")
