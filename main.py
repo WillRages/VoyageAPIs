ebfd745de1b0ceec11cd93cb6c67c1a0d6c2b114
Will Rages <90067249+WillRages@users.noreply.github.com>
2022-02-24 02:29:42 +0000

    made the list print better

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import requests
 from forms import hotelForm
 import json
-import sqlalchemy 
+import sqlalchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "csgod"
@@ -16,41 +16,11 @@ def homePage():
 
 @app.route("/hotel")
 def HotelPage():
-
     form = hotelForm()
-
-    if request.args.get("getHotels") == "y":
-
-        location = request.args.get("getPlace")
-
-        urlforhotels = "https://hotels4.p.rapidapi.com/locations/v2/search"
-
-        querystringforhotels = {
-            "query": location,  # you change the place name to your liking which is nice
-            "locale": "en_US",
-            "currency": "USD",
-        }
-
-        headersforhotel = {
-            "x-rapidapi-host": "hotels4.p.rapidapi.com",
-            "x-rapidapi-key": "4b44744771mshe890455adb53acdp173a42jsned37231cf971",
-        }
-
-        responseForHotel = requests.get(
-            urlforhotels, headers=headersforhotel, params=querystringforhotels
-        )
-        global hotelJSONdata
-        hotelJSONdata = responseForHotel.json()  # Do stuff with this data
-
-        with open("hotelData.json", "w+") as f:
-           f.write(responseForHotel.text) #make sure you write to the file
     return render_template("hotelform.html", form=form)
 
 @app.route("/hotelout",methods=["GET","POST"])
 def HotelOutput():
-
-    
-    print(request.values)
 
     if request.values.get("getHotels") == "y":
 
@@ -73,10 +43,19 @@ def HotelOutput():
             urlforhotels, headers=headersforhotel, params=querystringforhotels
         )
         
-        hotelJSONdata = responseForHotel.json()  # Do stuff with this data
+        response = responseForHotel.json()  # Do stuff with this data
+        hotels = []
+        for hotel in response["suggestions"][1]["entities"]:
+            hotels.append(hotel["name"])
 
-        #with open("hotelData.json", "w+") as f:
-        #   f.write(responseForHotel.text) #make sure you write to the file
-    return render_template("hotelOutput.html", Hotels = str(hotelJSONdata))
+        hotelJSONdata = hotels
+
+        
+        
+        with open("hotelData.json", "w+") as f:
+           f.write(responseForHotel.text) #make sure you write to the file
+    else:
+        hotelJSONdata = "no checkbox"
+    return render_template("hotelOutput.html", Hotels = hotelJSONdata)
 
 app.run(host="0.0.0.0")
